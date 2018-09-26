e47354637c3eb1ecfa60347f2e9448026c8ed4a3
liuyanli <liuyanli@weidian.com>
2018-09-26 16:32:00 +0800

    update

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,155 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+var generator = require('yeoman-generator'),
+    fs = require('fs'),
+    path = require('path'),
+    chalk = require('chalk')
 
-module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the kickass ' + chalk.red('generator-scaffold-cli') + ' generator!'
-    ));
+var config = {
+    github:{
+        username:'liuyanli',
+        repository:'scaffold',
+        branch:'master'
+    },
+    app:{
+        name:'scaffold-demo',
+        user:'liuyanli'
+	},
+	targetPath:''
+}
+var log = function(msg){
+    console.log(chalk.green(msg))
+}
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+module.exports = generator.Base.extend({
+    constructor(){
+        generator.Base.apply(this,arguments)
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+        
+        this.app = config.app
+        //add custom code
+        this.option("update", {
+            desc: "Update scaffold",
+            type: Boolean,
+            defaults: false
+        })
+		this.option("archive", {
+            desc: "The archive url",
+            type: String,
+            defaults: ""
+		})
+		this.option("username", {
+            desc: "The git username",
+            type: String,
+            defaults: config.github.username
+        })
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+        this.option("repository", {
+            desc: "The git repository name",
+            type: String,
+            defaults: config.github.repository
+        })
 
-  install() {
-    this.installDependencies();
-  }
-};
+        this.option("branch", {
+            desc: "The git branch name",
+            type: String,
+            defaults: config.github.branch
+		})
+		this.option("skip-local-cache", {
+            desc: "Skips the local cache",
+            type: Boolean,
+            defaults: true
+        })
+
+        this.option("skip-npm-install", {
+            desc: "Skips the installation of npm",
+            type: Boolean,
+            defaults: false
+        })
+
+        this.option("skip-bower-install", {
+            desc: "Skips the installation of bower",
+            type: Boolean,
+            defaults: false
+        })
+
+    },
+    // 初始化
+    initializing() {
+        log("Welcome to use scaffold.")
+    },
+    // 调用this.prompt()与用户产生交互
+    prompting(){
+        return this.prompt([{
+            type:'input',
+            name: 'appName',
+            message: '请输入项目名字',
+            default: config.app.name
+        },
+        {
+            type:'input',
+            name: 'userName',
+            message: '请输入创建者名字',
+            default: this.config.app.user
+        }]).then((answers)=>{
+            this.log('create project: ', answers.appName);
+            this.log('by: ', answers.userName);
+            this.app = answers;
+        })
+	},
+	//创建项目文件
+	writing(){
+		var self = this,
+			done = this.async(),
+			archive = this.options["archive"],
+            skipLocalCache = this.options["skip-local-cache"],
+            username = this.options["username"],
+            repository = this.options["repository"],
+            branch = this.options["branch"],
+			target = path.join(config.targetPath, config.app.name)
+			
+		function callback(err, remote){
+			var source
+			if (!err) {
+				log("The branch has been download to " + remote.cachePath)
+
+				log("Copy files to " + path.join(process.cwd(), target))
+
+				//copy assets and .* file
+				source = [path.join(remote.cachePath, "**"), path.join(remote.cachePath, ".*")]
+
+				fs.copy(source, target)
+
+			} else {
+				log(err)
+				process.exit(1)
+			}
+
+			done();
+		}
+		log("Downloading the files.");
+
+        //if archive is not empty
+        if(archive){
+            this.remote(archive, callback, skipLocalCache);
+        }else{
+            this.remote(username, repository, branch, callback, skipLocalCache);
+		}
+		
+	},
+	//调用(npm, bower)包install
+	install(){
+		var update = this.options["update"],
+		skipNpmInstall = this.options["skip-npm-install"],
+		skipBowerInstall = this.options["skip-bower-install"]
+
+		!update && this.installDependencies({
+			bower: skipBowerInstall ? false : true,
+			npm: skipNpmInstall ? false : true,
+			callback: function(err) {
+				log("The installation of dependencies has been download.");
+			}
+		})
+	},
+	end() {
+		log.info('generator success');
+	}
+})
